28235675d411525096637eb7436535b7555f3c56
ahong2028 <ahong2028@pingry.org>
2025-01-27 16:04:39 -0500

    Create SimpleProgram.py

---
+++ SimpleProgram.py
@@ -0,0 +1,11 @@
+# Albert Hong
+# Intro to Programming - Block 4
+# Simple Program 
+
+# Main Function
+def main():
+	# Printing the statement: This is a very simple "program" isn't it?
+	print('This is a very simple "program" isn\'t t it?')
+
+# Calling main]
+main()
